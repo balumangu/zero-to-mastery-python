19343d015eba36f63bd2c26b52d27117051201bd
Balakrishna Mangu <178907748+balumangu@users.noreply.github.com>
2026-02-24 15:04:49 +0530

    Python basics - Variables

---
+++ 01_basics/variables.py
@@ -0,0 +1,25 @@
+# ==============================================================================
+# SCRIPT: variables.py
+# PURPOSE: Fundamentals of Python Variables & Data Types
+# JOURNEY: Zero to Mastery - Python for DevOps
+# ==============================================================================
+
+# A variable is a container for a value. It can be a string. integer, float or boolean.
+
+# String
+first_name = "John"
+last_name = "Doe"
+city = "New York"
+
+# Integer
+age = 25
+weight = 80
+
+# Float
+temperature = 25.4
+height = 160.5
+
+# Boolean
+is_employed = False
+
+print(f"Hi {first_name} {last_name}. Welcome to {city} city. The temperature right now is {temperature}.")
