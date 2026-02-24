1eb2685ad757affc1136900ed46ede1313882bfc
Balakrishna Mangu <178907748+balumangu@users.noreply.github.com>
2026-02-24 16:15:34 +0530

    Python basics - User input and Type casting

---
+++ 01_basics/user_input_casting.py
@@ -0,0 +1,22 @@
+# ==============================================================================
+# SCRIPT: user_input_casting.py
+# PURPOSE: Handling user input and converting data types (Casting)
+# JOURNEY: Zero to Mastery - Python for DevOps
+# ==============================================================================
+
+
+# Import pi for calculation
+from math import pi
+
+# User input is always read as a string
+print("We are calculating perimeter of a rectangle and a circle.")
+
+raw_length = input("Please enter integer value for length: ")
+raw_height = input("Please enter integer value for height: ")
+raw_radius = input("Please enter value for radius: ")
+
+length = int(raw_length)
+height = int(raw_height)
+radius = float(raw_radius)
+
+print(f"The circunference of your rectangle is {length*height} and circle is {pi*radius}")
